989dd1bb6042f6118a261ff477a96143b0560486
DenisLatyshau <denislatyshau@yandex.ru>
2022-12-27 21:09:06 +0300

    feat: add first React components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Hello World')
+import React from "react";
+import ReactDOM from "react-dom";
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  )
+};
+
+const AppHeader = () => {
+  return <h1>My Todo list</h1>
+};
+
+const SearchPanel = () => {
+  return <input placeholder="search" />
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+  </div>
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
