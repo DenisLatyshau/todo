62c6b83af577755684caeca36c726e1900655581
DenisLatyshau <denislatyshau@yandex.ru>
2022-12-30 17:09:50 +0300

    feat: add TodoListItem

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,13 +2,17 @@ import React from "react";
 
 import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
+const TodoList = ( {todos} ) => {
+
+  const elements = todos.map((item) => {
+    return (
+      <li><TodoListItem {...item} /></li>
+    );
+  });
+
   return (
     <ul>
-      <li><TodoListItem label='Drink Coffee' /></li>
-      <li><TodoListItem 
-        label='Build Reack app'
-        important /></li>
+      {elements}
     </ul>
   )
 };
--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import TodoList from "./components/todo-list";
 import SearchPanel from "./components/search-panel";
 
 const App = () => {
+
+  const todoData = [
+    {label: 'Drink Coffee', important: false},
+    {label: 'Make Awesome App', important: true},
+    {label: 'Have a lunch', important: false}
+  ];
+
   return (
     <div>
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={todoData}/>
   </div>
   )
 };
