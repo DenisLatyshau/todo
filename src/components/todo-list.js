9b492ed7be152adbdfce970eb1412c6a6ebe160f
DenisLatyshau <denislatyshau@yandex.ru>
2023-01-09 23:07:22 +0300

    feat: start add css

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import TodoListItem from "./todo-list-item";
+import './todo-list.css';
 
 const TodoList = ( {todos} ) => {
 
@@ -9,14 +10,14 @@ const TodoList = ( {todos} ) => {
     const {id, ...itemProps} = item
 
     return (
-      <li key={id}>
+      <li key={id} className="list-group-item">
         <TodoListItem {...itemProps} />
       </li>
     );
   });
 
   return (
-    <ul>
+    <ul className="list-group todo-list">
       {elements}
     </ul>
   )
