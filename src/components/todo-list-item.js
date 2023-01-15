78da000d68526fc4afe243422433adcb4d897dc3
DenisLatyshau <denislatyshau@yandex.ru>
2023-01-15 21:44:37 +0300

    feat: add css

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,17 +1,33 @@
-import React from "react";
-import './todo-list-item.css'
+import React from 'react';
 
-const TodoListItem = ( { label, important = false } ) => {
+import './todo-list-item.css';
+
+const TodoListItem = ({ label, important = false }) => {
 
   const style = {
-    color: important ? 'tomato' : 'black'
-  }
+    color: important ? 'steelblue' : 'black',
+    fontWeight: important ? 'bold' : 'normal'
+  };
 
   return (
-    <span className="todo-list-item" 
-          style={style}>
-      { label }
-    </span>);
+    <span className="todo-list-item">
+      <span
+        className="todo-list-item-label"
+        style={style}>
+        {label}
+      </span>
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm float-right">
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
 };
 
 export default TodoListItem;
