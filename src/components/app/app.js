46f3956b28e1b31655734d87eb41824d2662d5e9
DenisLatyshau <denislatyshau@yandex.ru>
2023-01-25 21:33:57 +0300

    feat: add addItem()

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,10 +4,13 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
 export default class App extends Component {
+
+  maxId = 100;
 
   state = {
     todoData: [
@@ -28,6 +31,20 @@ export default class App extends Component {
     })
   }
 
+  addItem = (text) => {
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    }
+    this.setState(({ todoData }) => {
+      const newArray = [...todoData, newItem]
+      return {
+        todoData: newArray
+      }
+    })
+  }
+
   render () {
     return (
       <div className="todo-app">
@@ -40,6 +57,7 @@ export default class App extends Component {
         <TodoList 
           todos={this.state.todoData}
           onDeleted={ this.deleteItem } />
+        <ItemAddForm onItemAdded={this.addItem} />
       </div>
     );
   };
