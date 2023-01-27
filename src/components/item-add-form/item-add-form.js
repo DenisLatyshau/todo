342b05d67a68fc806a2ce2e9820a4d7a6ec28b61
DenisLatyshau <denislatyshau@yandex.ru>
2023-01-27 20:48:17 +0300

    feat: add onChange() in form

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,18 +2,36 @@ import React, { Component } from "react";
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+
+  state = {
+    label: ''
+  }
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.onItemAdded(this.state.label)
+  }
+
   render () {
 
-    const { onItemAdded } = this.props
-
     return (
-      <div className="item-add-form">
+      <form className="item-add-form d-flex"
+            onSubmit={this.onSubmit}>
+        <input type='text' 
+              className="form-control"
+              onChange={this.onLabelChange}
+              placeholder='What needs to be done' />
         <button 
-          className="btn btn-outline-secondary"
-          onClick={() => onItemAdded('Hello World')}>
+          className="btn btn-outline-secondary">
           Add Item
         </button>
-      </div>
+      </form>
     )
   } 
 }
