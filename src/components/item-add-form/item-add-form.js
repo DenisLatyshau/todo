0db5701fa774a57ff8587381496c9dc4d7845d7d
DenisLatyshau <denislatyshau@yandex.ru>
2023-01-27 20:54:30 +0300

    feat: add value in form

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -16,6 +16,9 @@ export default class ItemAddForm extends Component {
   onSubmit = (e) => {
     e.preventDefault()
     this.props.onItemAdded(this.state.label)
+    this.setState({
+      label: ''
+    })
   }
 
   render () {
@@ -26,7 +29,8 @@ export default class ItemAddForm extends Component {
         <input type='text' 
               className="form-control"
               onChange={this.onLabelChange}
-              placeholder='What needs to be done' />
+              placeholder='What needs to be done' 
+              value={this.state.label} />
         <button 
           className="btn btn-outline-secondary">
           Add Item
